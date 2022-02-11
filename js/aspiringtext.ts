8239c4fba70b09f4d0c4e163de550a372893ded9
frumsrekovni <joshua.backstrom@gmail.com>
2022-02-12 00:04:59 +0100

    The script actually changes the textContent now and runs at an interval

--- js/aspiringtext.ts
+++ js/aspiringtext.ts
@@ -1,9 +1,10 @@
 
+setInterval(_get_Random_Word, 5000);
 // Function that return one random word from the list of words
 function _get_Random_Word(){
     const aspiring_words = ['Aspiring','Dynamic','Enthusiastic' ,'Endeavoring','Eager','Striving','Ambitious','Diligent','Keen','Raring','Zealous','Aspirant','Wishful','Determined'];
-    //const element = document.getElementById("main_page_aspiring_text");
-    //element.textContent = aspiring_words[Math.floor(Math.random() * aspiring_words.length)]; 
+    const element = document.getElementById("main_page_aspiring_text");
+    element.textContent = aspiring_words[Math.floor(Math.random() * aspiring_words.length)]; 
     return aspiring_words[Math.floor(Math.random() * aspiring_words.length)]; 
 }
 console.log(_get_Random_Word());
