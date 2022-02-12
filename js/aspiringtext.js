22e38039a35a61d98c87f340ebc8b20bc8914e27
frumsrekovni <joshua.backstrom@gmail.com>
2022-02-12 22:07:19 +0100

    Animation of cycling through aspiring words now works as intended

--- js/aspiringtext.js
+++ js/aspiringtext.js
@@ -1,9 +1,13 @@
-setInterval(_get_Random_Word, 5000);
-// Function that return one random word from the list of words
+// Changes aspiring text to a random word in the list and triggers the CSS animation
+var element = document.getElementById("main_page_aspiring_text");
+setInterval(_get_Random_Word, 5200);
 function _get_Random_Word() {
     var aspiring_words = ['Aspiring', 'Dynamic', 'Enthusiastic', 'Endeavoring', 'Eager', 'Striving', 'Ambitious', 'Diligent', 'Keen', 'Raring', 'Zealous', 'Aspirant', 'Wishful', 'Determined'];
-    var element = document.getElementById("main_page_aspiring_text");
     element.textContent = aspiring_words[Math.floor(Math.random() * aspiring_words.length)];
-    return aspiring_words[Math.floor(Math.random() * aspiring_words.length)];
+    element.classList.add('fadeIn_fadeOut');
 }
-console.log(_get_Random_Word());
+;
+element.onanimationend = function () {
+    element.style.opacity = '0';
+    element.classList.remove('fadeIn_fadeOut');
+};
--- js/aspiringtext.ts
+++ js/aspiringtext.ts
@@ -1,10 +1,17 @@
 
-setInterval(_get_Random_Word, 5000);
-// Function that return one random word from the list of words
+
+// Changes aspiring text to a random word in the list and triggers the CSS animation
+const element = document.getElementById("main_page_aspiring_text");
+
+setInterval(_get_Random_Word, 5200);
+
 function _get_Random_Word(){
     const aspiring_words = ['Aspiring','Dynamic','Enthusiastic' ,'Endeavoring','Eager','Striving','Ambitious','Diligent','Keen','Raring','Zealous','Aspirant','Wishful','Determined'];
-    const element = document.getElementById("main_page_aspiring_text");
-    element.textContent = aspiring_words[Math.floor(Math.random() * aspiring_words.length)]; 
-    return aspiring_words[Math.floor(Math.random() * aspiring_words.length)]; 
-}
-console.log(_get_Random_Word());
+    element.textContent = aspiring_words[Math.floor(Math.random() * aspiring_words.length)];
+    element.classList.add('fadeIn_fadeOut');
+};
+
+element.onanimationend = () => {
+    element.style.opacity = '0';
+    element.classList.remove('fadeIn_fadeOut');
+};
