f604e4942c64d5cb7bc1bd917f624c0a1f440da4
frumsrekovni <joshua.backstrom@gmail.com>
2022-04-02 18:34:25 +0200

    Added a blocker div that makes it possible to close the redirects tab by clicking outside it. TODO: make it work with closing a project expansion aswell

--- js/projects_page.ts
+++ js/projects_page.ts
@@ -1,5 +1,8 @@
 
 const project_element = document.getElementById("pomodoro_timer_project");
+const body_element = document.getElementsByTagName("body");
+const nav_bar_redirects_checkbox = document.getElementById("hamburger_menu_toggle") as HTMLInputElement;
+const redirect_blocker = document.getElementById("all_directs_blocker");
 
 
 project_element.addEventListener("click", () => {
@@ -11,4 +14,15 @@ project_element.addEventListener("click", () => {
         project_element.style.width = "1000px";
         project_element.style.height = "1000px";
        // project_element.innerText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+});
+
+nav_bar_redirects_checkbox.addEventListener("click", () => {
+    if(nav_bar_redirects_checkbox.checked){
+        redirect_blocker.style.display = "flex";
+    };
+});
+
+redirect_blocker.addEventListener("click", () => {
+    nav_bar_redirects_checkbox.checked = false;
+    redirect_blocker.style.display = "none";
 });
