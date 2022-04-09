0dfab44585a164c383693956ded0165fa632aa25
frumsrekovni <joshua.backstrom@gmail.com>
2022-04-09 14:41:10 +0200

    Shows full Email when the 'Email me!' text is hovered. And when pressed it copies full email to clipboard and shows an alert for 1s

--- js/aspiringtext.ts
+++ js/aspiringtext.ts
@@ -6,7 +6,7 @@ const element = document.getElementById("main_page_aspiring_text");
 setInterval(_get_Random_Word, 5200);
 
 function _get_Random_Word(){
-    const aspiring_words = ['Ambitious','Aspiring', 'Aspirant','Avid','Ardent','Dynamic','Determined','Driven','Diligent','Enthusiastic' ,'Endeavoring','Fervent','Eager','Keen','Raring','Ready','Resolved','Striving','Self-seeking','Go-getting','Passionate','Zealous','Wishful','Willing'];
+    const aspiring_words = ['Ambitious','Aspiring', 'Aspirant','Avid','Ardent','C/C++','Dynamic','Determined','Driven','Diligent','Enthusiastic' ,'Endeavoring','Fervent','Eager','Keen','Raring','Ready','Resolved','Striving','Self-seeking','Tenacious','Typescript','Go-getting','Passionate','Python','Zealous','Wishful','Willing'];
     element.textContent = aspiring_words[Math.floor(Math.random() * aspiring_words.length)];
     element.classList.add('fadeIn_fadeOut');
 };
@@ -15,3 +15,13 @@ element.onanimationend = () => {
     element.style.opacity = '0';
     element.classList.remove('fadeIn_fadeOut');
 };
+
+function copyEmail(){
+    navigator.clipboard.writeText("joshua.backstrom@gmail.com");
+
+    const alertbox = document.createElement("div");
+    alertbox.classList.add("copy_alert");
+    alertbox.innerHTML = "Email Copied!";
+    setTimeout(function(){alertbox.parentNode.removeChild(alertbox);},1000);
+    document.body.appendChild(alertbox);
+}
