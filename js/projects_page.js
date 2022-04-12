a699a445298a4f6135132a3fe0c62037195382b0
frumsrekovni <joshua.backstrom@gmail.com>
2022-04-12 23:31:58 +0200

    Changed appearance of projects page. the image is now the background image and not a separate img element

--- js/projects_page.js
+++ js/projects_page.js
@@ -1,10 +1,24 @@
 var project_element = document.getElementById("pomodoro_timer_project");
 var body_element = document.getElementsByTagName("body");
+var active_project_blocker = document.getElementById("out_of_bound_blocker");
+var project_toggle = document.getElementById("pomodoro_timer_project_toggle");
 project_element.addEventListener("click", function () {
-    project_element.classList.add("active_project");
-    project_element.innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/TXXjeIF7M7I\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
-    var node = document.createElement("div");
-    node.classList.add("text_for_a_project");
-    node.innerText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-    project_element.appendChild(node);
+    // project_element.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/TXXjeIF7M7I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    // const node = document.createElement("div");
+    // node.classList.add("text_for_a_project");
+    // node.innerText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    // project_element.appendChild(node);
+    active_project_blocker.style.display = "flex";
+});
+// nav_bar_redirects_checkbox.addEventListener("click", () => {
+//     if(nav_bar_redirects_checkbox.checked){
+//         redirect_blocker.style.display = "flex";
+//     }
+//     else{
+//         redirect_blocker.style.display = "none";
+//     }
+// });
+active_project_blocker.addEventListener("click", function () {
+    project_toggle.checked = false;
+    active_project_blocker.style.display = "none";
 });
--- js/projects_page.ts
+++ js/projects_page.ts
@@ -1,13 +1,29 @@
 
 const project_element = document.getElementById("pomodoro_timer_project");
 const body_element = document.getElementsByTagName("body");
+const active_project_blocker = document.getElementById("out_of_bound_blocker");
+const project_toggle = document.getElementById("pomodoro_timer_project_toggle") as HTMLInputElement;
 
 
 project_element.addEventListener("click", () => {
-        project_element.classList.add("active_project");
-        project_element.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/TXXjeIF7M7I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-        const node = document.createElement("div");
-        node.classList.add("text_for_a_project");
-        node.innerText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-        project_element.appendChild(node);
+        // project_element.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/TXXjeIF7M7I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+        // const node = document.createElement("div");
+        // node.classList.add("text_for_a_project");
+        // node.innerText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        // project_element.appendChild(node);
+        active_project_blocker.style.display = "flex";
+});
+
+// nav_bar_redirects_checkbox.addEventListener("click", () => {
+//     if(nav_bar_redirects_checkbox.checked){
+//         redirect_blocker.style.display = "flex";
+//     }
+//     else{
+//         redirect_blocker.style.display = "none";
+//     }
+// });
+
+active_project_blocker.addEventListener("click", () => {
+    project_toggle.checked = false;
+    active_project_blocker.style.display = "none";
 });
