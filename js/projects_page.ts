a28ce9e6441c4bb986f159edb41bc088c7d872ff
frumsrekovni <joshua.backstrom@gmail.com>
2022-04-14 18:46:53 +0200

    Added more interactable projects on the projects page. The skywriter shows a gif when hovered.

--- js/projects_page.ts
+++ js/projects_page.ts
@@ -1,19 +1,35 @@
 
-const project_element = document.getElementById("pomodoro_timer_project");
-const body_element = document.getElementsByTagName("body");
+const pomodoro_timer_project_element = document.getElementById("pomodoro_timer_project");
+const portfolio_project_element = document.getElementById("portfolio_project");
+const skywriter_project_element = document.getElementById("skywriter_project");
+
+
+const pomodoro_timer_project_toggle = document.getElementById("pomodoro_timer_project_toggle") as HTMLInputElement;
+const portfolio_project_element_toggle = document.getElementById("portfolio_toggle") as HTMLInputElement;
+const skywriter_project_element_toggle = document.getElementById("skywriter_toggle") as HTMLInputElement;
+
 const active_project_blocker = document.getElementById("out_of_bound_blocker");
-const project_toggle = document.getElementById("pomodoro_timer_project_toggle") as HTMLInputElement;
+const all_project_checkboxes = document.getElementsByClassName("project_checkbox") as HTMLCollectionOf<HTMLInputElement>;
 
+function disableAllCheckboxes(){
 
-project_element.addEventListener("click", () => {
-        // project_element.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/TXXjeIF7M7I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-        // const node = document.createElement("div");
-        // node.classList.add("text_for_a_project");
-        // node.innerText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-        // project_element.appendChild(node);
-        project_toggle.disabled = true;
-        project_toggle.checked = true;
-        active_project_blocker.style.display = "flex";
+    for (let i = 0; i < all_project_checkboxes.length; i++) {
+        all_project_checkboxes[i].disabled = true;
+      }
+}
+
+// The repetition here is real ugly but in order make the project be able to expand and not shrink when pressed the project checkboxes unfortunately needs to disabled and handled with javascript.
+pomodoro_timer_project_element.addEventListener("click", () => {
+    pomodoro_timer_project_toggle.checked = true;
+    active_project_blocker.style.display = "flex";
+});
+portfolio_project_element.addEventListener("click", () => {
+    portfolio_project_element_toggle.checked = true;
+    active_project_blocker.style.display = "flex";
+});
+skywriter_project_element.addEventListener("click", () => {
+    skywriter_project_element_toggle.checked = true;
+    active_project_blocker.style.display = "flex";
 });
 
 // nav_bar_redirects_checkbox.addEventListener("click", () => {
@@ -26,6 +42,8 @@ project_element.addEventListener("click", () => {
 // });
 
 active_project_blocker.addEventListener("click", () => {
-    project_toggle.checked = false;
+    for (let i = 0; i < all_project_checkboxes.length; i++) {
+        all_project_checkboxes[i].checked = false;
+      }
     active_project_blocker.style.display = "none";
 });
