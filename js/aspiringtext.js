dd5a2207cd92a94ba0888a4b5cf58110f03936d2
frumsrekovni <joshua.backstrom@gmail.com>
2022-04-26 23:47:34 +0200

    Added as many childhood shows and games that I can remember to the landing page

--- js/aspiringtext.js
+++ js/aspiringtext.js
@@ -15,12 +15,19 @@ element.onanimationend = function () {
     element.classList.remove('fadeIn_fadeOut');
 };
 function _get_Random_Tv_Show() {
-    var tv_shows = ['One Piece', 'Naruto', 'Xena: Warrior Princess', 'TaleSpin', 'Sailor Moon', 'REA', 'Johnny Bravo', 'Ed, Edd n Eddy', 'Samurai Jack', "Dexter's Laboratory", 'Hjärnkontoret', 'Myror i brallan', 'Björnes magasin', 'Fångarna på fortet', 'The Fresh Prince of Bel-Air'];
+    var tv_shows = ['One Piece', 'Naruto', 'Xena: Warrior Princess', 'TaleSpin', 'Sailor Moon', 'REA', 'Johnny Bravo',
+        'Ed, Edd n Eddy', 'Samurai Jack', "Dexter's Laboratory", 'Hjärnkontoret', 'Myror i brallan', 'Björnes magasin',
+        'Fångarna på fortet', 'The Fresh Prince of Bel-Air', 'Lloyd i rymden', 'Anki & Pytte',
+        'Nalle har ett stort blått hus', 'Dinosaurs'];
     el_tvshow.textContent = tv_shows[Math.floor(Math.random() * tv_shows.length)];
 }
 ;
 function _get_Random_Game() {
-    var childhood_games = ['counter-strike', 'battlefront', 'halo', 'runescape', 'pokemon sapphire', 'sherwood dungeon', 'battleon', 'guitar hero', 'san andreas'];
+    var childhood_games = ['counter-strike', 'battlefront', 'halo', 'runescape', 'pokemon sapphire', 'pokemon red', 'sherwood dungeon',
+        'battleon', 'guitar hero', 'san andreas', 'tekken 3', 'dark rift', 'bio freaks', 'super battletank 2', "kirby's dream course",
+        'super mario all-stars', 'super mario 64', 'super mario kart', 'zelda link to the past', 'donkey kong country 2', 'time crisis',
+        'theme park world', 'dune', 'gta 2', 'NHL 99', 'fifa 99', 'worms armageddon', 'digimon world', 'red card', 'road rash jailbreak',
+        'red alert', 'hugo 2', 'medal of honor', 'quake', 'warpath jurassic park', "anna kournikova's smash court tennis", 'dr. mario'];
     el_game.textContent = childhood_games[Math.floor(Math.random() * childhood_games.length)];
 }
 ;
