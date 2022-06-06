4aafa714d10122ff2f9de7b56cba8da8f5698b00
frumsrekovni <joshua.backstrom@gmail.com>
2022-06-06 08:56:56 +0200

    Color pickers are more responsive. Added hidden color pickers for the nav bar that activates with doubleclick.

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,16 @@
+const nav_bar = document.getElementsByTagName("nav")[0];
 const nav_bar_redirects_checkbox = document.getElementById("hamburger_menu_toggle");
 const redirect_blocker = document.getElementById("out_of_bound_blocker");
 const bgColor = document.getElementById("bgcolor");
 const midBgColor = document.getElementById("bgcolormid");
+const leftbarbgColor = document.getElementById("leftbar");
+const rightbarbgColor = document.getElementById("rightbar");
+function changenavbg() {
+    const hiddenpickers = document.getElementsByClassName("hiddenpicker");
+    for (let i = 0; i < hiddenpickers.length; i++) {
+        hiddenpickers[i].style.display = "flex";
+    }
+}
 nav_bar_redirects_checkbox.addEventListener("click", () => {
     if (nav_bar_redirects_checkbox.checked) {
         redirect_blocker.style.display = "flex";
@@ -22,9 +31,16 @@ function copyEmail() {
     setTimeout(function () { alertbox.parentNode.removeChild(alertbox); }, 1000);
     document.body.appendChild(alertbox);
 }
-bgColor.addEventListener("change", function () {
+bgColor.addEventListener("input", function () {
+    nav_bar.style.background = "linear-gradient(90deg, " + this.value + " 5%, " + leftbarbgColor.value + " 30%, " + rightbarbgColor.value + " 70%, " + this.value + " 95%)";
     document.body.style.background = "linear-gradient(180deg, " + this.value + " 0%, " + midBgColor.value + " 15%," + midBgColor.value + " 85%," + this.value + " 100%)";
 });
-midBgColor.addEventListener("change", function () {
+midBgColor.addEventListener("input", function () {
     document.body.style.background = "linear-gradient(180deg, " + bgColor.value + " 0%, " + this.value + " 15%," + this.value + " 85%," + bgColor.value + " 100%)";
+});
+leftbarbgColor.addEventListener("input", function () {
+    nav_bar.style.background = "linear-gradient(90deg, " + bgColor.value + " 5%, " + this.value + " 30%, " + rightbarbgColor.value + " 70%, " + bgColor.value + " 95%)";
+});
+rightbarbgColor.addEventListener("input", function () {
+    nav_bar.style.background = "linear-gradient(90deg, " + bgColor.value + " 5%, " + leftbarbgColor.value + " 30%, " + this.value + " 70%, " + bgColor.value + " 95%)";
 });
--- js/navbar.ts
+++ js/navbar.ts
@@ -1,7 +1,18 @@
+const nav_bar = document.getElementsByTagName("nav")[0];
 const nav_bar_redirects_checkbox = document.getElementById("hamburger_menu_toggle") as HTMLInputElement;
 const redirect_blocker = document.getElementById("out_of_bound_blocker");
 const bgColor = document.getElementById("bgcolor") as HTMLInputElement
 const midBgColor = document.getElementById("bgcolormid") as HTMLInputElement
+const leftbarbgColor = document.getElementById("leftbar") as HTMLInputElement
+const rightbarbgColor = document.getElementById("rightbar") as HTMLInputElement
+
+function changenavbg(){
+    const hiddenpickers = document.getElementsByClassName("hiddenpicker") as HTMLCollectionOf<HTMLElement>;
+    for (let i = 0; i < hiddenpickers.length; i++) {
+        hiddenpickers[i].style.display = "flex";
+    }
+}
+
 
 nav_bar_redirects_checkbox.addEventListener("click", () => {
     if(nav_bar_redirects_checkbox.checked){
@@ -26,9 +37,17 @@ function copyEmail(){
     document.body.appendChild(alertbox);
 }
 
-bgColor.addEventListener("change", function() {
+bgColor.addEventListener("input", function() {
+    nav_bar.style.background = "linear-gradient(90deg, "+this.value+" 5%, "+leftbarbgColor.value+" 30%, "+rightbarbgColor.value+" 70%, "+this.value+" 95%)";
     document.body.style.background ="linear-gradient(180deg, "+(this as HTMLInputElement).value+" 0%, "+midBgColor.value+" 15%,"+midBgColor.value+" 85%,"+(this as HTMLInputElement).value+" 100%)";
 });
-midBgColor.addEventListener("change", function() {
+midBgColor.addEventListener("input", function() {
     document.body.style.background ="linear-gradient(180deg, "+bgColor.value+" 0%, "+(this as HTMLInputElement).value+" 15%,"+(this as HTMLInputElement).value+" 85%,"+bgColor.value+" 100%)";
+});
+
+leftbarbgColor.addEventListener("input", function() {
+    nav_bar.style.background = "linear-gradient(90deg, "+bgColor.value+" 5%, "+this.value+" 30%, "+rightbarbgColor.value+" 70%, "+bgColor.value+" 95%)";
+});
+rightbarbgColor.addEventListener("input", function() {
+    nav_bar.style.background = "linear-gradient(90deg, "+bgColor.value+" 5%, "+leftbarbgColor.value+" 30%, "+this.value+" 70%, "+bgColor.value+" 95%)";
 });
